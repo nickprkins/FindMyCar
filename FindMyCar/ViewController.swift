e155411a6f644a3638f95759b4653c6d0ef39214
Nick Perkins <nickperkins@protonmail.com>
2016-05-15 21:44:59 -0400

    Fixed polyline drawing, added custom car annotation pin

--- FindMyCar/ViewController.swift
+++ FindMyCar/ViewController.swift
@@ -14,20 +14,24 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
 
     @IBOutlet weak var mapView: MKMapView!
     var locationManager: CLLocationManager!
+    var currentLocation : CLLocation!
     var previousLocation : CLLocation!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.title = "Find My Car"
+        
         locationManager = CLLocationManager()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.delegate = self
         
+        
+        // present an alert indicating location authorization required
+        // and offer to take the user to Settings for the app
         let status = CLLocationManager.authorizationStatus()
         if status == .NotDetermined || status == .Denied || status == .AuthorizedWhenInUse {
-            // present an alert indicating location authorization required
-            // and offer to take the user to Settings for the app via
-            // UIApplication -openUrl: and UIApplicationOpenSettingsURLString
+            
             locationManager.requestAlwaysAuthorization()
             locationManager.requestWhenInUseAuthorization()
         }
@@ -38,65 +42,80 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         mapView.delegate = self
         mapView.showsUserLocation = true
         mapView.mapType = MKMapType(rawValue: 0)!
-        mapView.userTrackingMode = MKUserTrackingMode(rawValue: 2)!
+        mapView.userTrackingMode = MKUserTrackingMode(rawValue: 1)!
         
     }
 
     @IBAction func addMyCar(sender: AnyObject) {
         
-        let location = CLLocationCoordinate2DMake(40.730872, -74.003066)
+        let location = CLLocationCoordinate2DMake(locationManager!.location!.coordinate.latitude, locationManager!.location!.coordinate.longitude)
         
         let annotation = MKPointAnnotation()
         annotation.coordinate = location
         annotation.title = "Dude..Sweet..My Car!"
-        annotation.subtitle = "Your car is here dude. Don't drive while intoxicated dude."
+        annotation.subtitle = "Your car is here dude. Be sober dude!"
+//        annotation.image = UIImage (named: "mapSmallIconRed")
+    
         
         //mapView(mapView, viewForAnnotation: annotation)!.annotation = annotation
+        
         mapView.addAnnotation(annotation)
 
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    // CCLocationManager Delegate. Inside a call is made to have a polyline drawn.
     func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation){
         
-        if let oldLocationNew = oldLocation as CLLocation?{
-            let oldCoordinates = oldLocationNew.coordinate
-            let newCoordinates = newLocation.coordinate
-            var area = [oldCoordinates, newCoordinates]
-            let polyline = MKPolyline(coordinates: &area, count: area.count)
-            mapView.addOverlay(polyline)
+        let oldCoordinates = oldLocation.coordinate
+        let newCoordinates = newLocation.coordinate
+        
+        // This is done to keep it from writing a line from 0,0 which is 389.65 miles off the coast of Ghana in Africa.
+        if oldCoordinates.latitude != 0 {
+        
+            if let oldLocationNew = oldLocation as CLLocation?{
+                var area = [oldCoordinates, newCoordinates]
+                let polyline = MKPolyline(coordinates: &area, count: area.count)
+                mapView.addOverlay(polyline)
+            }
         }
     
     }
-    
+    // this function draws the polyline on the map.
     func mapView(mapView: MKMapView, rendererForOverlay overlay: MKOverlay) -> MKOverlayRenderer {
         if (overlay is MKPolyline) {
             let pr = MKPolylineRenderer(overlay: overlay)
-            pr.strokeColor = UIColor.redColor()
+            pr.strokeColor = UIColor.blueColor()
             pr.lineWidth = 5
             return pr
         }
         return MKPolylineRenderer()
     }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
     // MARK: - MapView Delegate
-//    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
-//        let annotationReuseId = "Place"
-//        var mapView = mapView.dequeueReusableAnnotationViewWithIdentifier(annotationReuseId)
-//        if mapView == nil {
-//            mapView = MKAnnotationView(annotation: annotation, reuseIdentifier: annotationReuseId)
-//        } else {
-//            mapView!.annotation = annotation
-//        }
-//        mapView!.image = UIImage(named: "mapIconSmallRed")
-//        mapView!.backgroundColor = UIColor.clearColor()
-//        mapView!.canShowCallout = true
-//        return mapView
-//    }
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        if !(annotation is MKPointAnnotation) {
+            return nil
+        }
+        
+        var annotationView = mapView.dequeueReusableAnnotationViewWithIdentifier("Car")
+        if annotationView == nil {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "Car")
+            annotationView!.canShowCallout = true
+        }
+        else {
+            annotationView!.annotation = annotation
+        }
+        
+        annotationView!.image = UIImage(named: "mapIconSmallRed")
+        
+        return annotationView
+    }
 }
+
+
 
